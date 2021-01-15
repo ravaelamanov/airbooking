b2e515488f7af3dc8fe0d6fc0bbd15ce617589f6
ravaelamanov <ravaelamanov@gmail.com>
2021-01-16 00:26:59 +0300

    init project

---
+++ src/test/java/com/airbooking/AirbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
