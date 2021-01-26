0cd342f3d903f9a65892c67b6b0fe737e9b57e29
ravaelamanov <ravaelamanov@gmail.com>
2021-01-27 02:44:04 +0300

    Added ManyToOne airplane mapping

--- src/main/java/com/airbooking/da/entities/Flight.java
+++ src/main/java/com/airbooking/da/entities/Flight.java
@@ -34,8 +34,79 @@ public class Flight {
     @Column(nullable = false)
     private String arrivalCountry;
 
-    @Column(nullable = false)
+    @ManyToOne
     private Airplane airplane;
 
 
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getFlightNo() {
+        return FlightNo;
+    }
+
+    public void setFlightNo(String flightNo) {
+        FlightNo = flightNo;
+    }
+
+    public LocalDateTime getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(LocalDateTime departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public LocalDateTime getArrivalDateTime() {
+        return arrivalDateTime;
+    }
+
+    public void setArrivalDateTime(LocalDateTime arrivalDateTime) {
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureCountry() {
+        return departureCountry;
+    }
+
+    public void setDepartureCountry(String departureCountry) {
+        this.departureCountry = departureCountry;
+    }
+
+    public String getArrivalCountry() {
+        return arrivalCountry;
+    }
+
+    public void setArrivalCountry(String arrivalCountry) {
+        this.arrivalCountry = arrivalCountry;
+    }
+
+    public Airplane getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(Airplane airplane) {
+        this.airplane = airplane;
+    }
 }
