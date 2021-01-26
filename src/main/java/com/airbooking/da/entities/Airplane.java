e3025d948dc0df04e8015d2467f3e9ae80c82ee9
ravaelamanov <ravaelamanov@gmail.com>
2021-01-27 02:05:44 +0300

    Added Airplane.java

---
+++ src/main/java/com/airbooking/da/entities/Airplane.java
@@ -0,0 +1,46 @@
+package com.airbooking.da.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "AIRPLANES")
+public class Airplane {
+    @Id
+    @GeneratedValue(generator = "incrementor")
+    @GenericGenerator(name = "incrementor", strategy = "increment")
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "airplane")
+    private List<Seat> seats;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
+}
