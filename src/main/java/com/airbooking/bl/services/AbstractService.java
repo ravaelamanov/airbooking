b2fcd4e78231c745c89838b20ff9e5428fe45642
ravaelamanov <ravaelamanov@gmail.com>
2021-01-28 01:21:51 +0300

    replaced ModelMapper with IMapper

--- src/main/java/com/airbooking/bl/services/AbstractService.java
+++ src/main/java/com/airbooking/bl/services/AbstractService.java
@@ -1,18 +1,16 @@
 package com.airbooking.bl.services;
 
+import com.airbooking.bl.mappers.IMapper;
 import com.airbooking.exceptions.ResourceNotFoundException;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public abstract class AbstractService<Dto, Entity, ID> implements IService<Dto, ID> {
 
     @Autowired
-    protected ModelMapper modelMapper;
+    protected IMapper<Dto, Entity> modelMapper;
 
     protected CrudRepository<Entity, ID> repository;
 
@@ -22,7 +20,7 @@ public abstract class AbstractService<Dto, Entity, ID> implements IService<Dto, 
 
     @Override
     public Iterable<Dto> findAll() {
-        return toDto(repository.findAll());
+        return modelMapper.toDto(repository.findAll());
     }
 
     @Override
@@ -33,44 +31,24 @@ public abstract class AbstractService<Dto, Entity, ID> implements IService<Dto, 
         } catch (NoSuchElementException exception) {
             throw new ResourceNotFoundException("Entity not found. Entity id: " + id.toString());
         }
-        return toDto(entity);
+        return modelMapper.toDto(entity);
     }
 
     @Override
     public Dto create(Dto dto) {
-        Entity entity = toEntity(dto);
-        return toDto(repository.save(entity));
+        Entity entity = modelMapper.toEntity(dto);
+        return modelMapper.toDto(repository.save(entity));
     }
 
     @Override
     public Dto update(Dto dto) {
-        Entity entity = toEntity(dto);
-        return toDto(repository.save(entity));
+        Entity entity = modelMapper.toEntity(dto);
+        return modelMapper.toDto(repository.save(entity));
     }
 
     @Override
     public void deleteById(ID id) {
         repository.deleteById(id);
-    }
-
-    protected abstract Dto toDto(Entity entity);
-
-    protected abstract Entity toEntity(Dto dto);
-
-    protected Iterable<Entity> toEntity(Iterable<Dto> dtos) {
-        List<Entity> users = new ArrayList<>();
-        for (Dto dto : dtos) {
-            users.add(toEntity(dto));
-        }
-        return users;
-    }
-
-    protected Iterable<Dto> toDto(Iterable<Entity> entities) {
-        List<Dto> dtos = new ArrayList<>();
-        for (Entity entity : entities) {
-            dtos.add(toDto(entity));
-        }
-        return dtos;
     }
 
 }
