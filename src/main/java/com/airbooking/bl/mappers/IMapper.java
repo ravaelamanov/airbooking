4981fce3d2258867797a11c03b2f5103cc6113dd
ravaelamanov <ravaelamanov@gmail.com>
2021-01-28 01:24:03 +0300

    added IMapper.java

---
+++ src/main/java/com/airbooking/bl/mappers/IMapper.java
@@ -0,0 +1,25 @@
+package com.airbooking.bl.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface IMapper<Dto, Entity> {
+    Dto toDto(Entity entity);
+    Entity toEntity(Dto dto);
+
+    default Iterable<Entity> toEntity(Iterable<Dto> dtos) {
+        List<Entity> users = new ArrayList<>();
+        for (Dto dto : dtos) {
+            users.add(toEntity(dto));
+        }
+        return users;
+    }
+
+    default Iterable<Dto> toDto(Iterable<Entity> entities) {
+        List<Dto> dtos = new ArrayList<>();
+        for (Entity entity : entities) {
+            dtos.add(toDto(entity));
+        }
+        return dtos;
+    }
+}
