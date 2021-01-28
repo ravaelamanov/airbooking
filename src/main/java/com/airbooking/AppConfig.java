14526870bfd66882ddea18d1f9efc7b00b719d02
ravaelamanov <ravaelamanov@gmail.com>
2021-01-28 22:34:41 +0300

    set modelMapper matching strategy property to STRICT

--- src/main/java/com/airbooking/AppConfig.java
+++ src/main/java/com/airbooking/AppConfig.java
@@ -1,9 +1,14 @@
 package com.airbooking;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @ComponentScan
@@ -13,7 +18,8 @@ public class AppConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
                 .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
     }
 }
