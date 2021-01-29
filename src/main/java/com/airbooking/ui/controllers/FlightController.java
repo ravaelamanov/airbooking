a5a256decd51a53ed53c4f0b6202533f0faacc1a
ravaelamanov <ravaelamanov@gmail.com>
2021-01-29 19:26:34 +0300

    added airplaneService, corrected return types

--- src/main/java/com/airbooking/ui/controllers/FlightController.java
+++ src/main/java/com/airbooking/ui/controllers/FlightController.java
@@ -1,9 +1,15 @@
 package com.airbooking.ui.controllers;
 
+import com.airbooking.bl.services.AirplaneService;
 import com.airbooking.bl.services.FlightService;
+import com.airbooking.ui.models.response.FlightResponseModel;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("flights")
@@ -12,16 +18,25 @@ public class FlightController {
     @Autowired
     private FlightService flightService;
 
+    @Autowired
+    private AirplaneService airplaneService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public void getFlights() {
+    public List<FlightResponseModel> getFlights() {
+        List<FlightResponseModel> responseModels = new ArrayList<>();
+        flightService.findAll().forEach(flightDto -> {responseModels.add(modelMapper.map(flightDto, FlightResponseModel.class));});
+        return responseModels;
 
     }
 
     @GetMapping(path = "/{flightId}")
     @ResponseStatus(HttpStatus.OK)
-    public void getFlight(@PathVariable Long flightId) {
-
+    public FlightResponseModel getFlight(@PathVariable Long flightId) {
+        return modelMapper.map(flightService.findById(flightId), FlightResponseModel.class);
     }
 
     @PostMapping
@@ -39,6 +54,5 @@ public class FlightController {
     @DeleteMapping(path = "/{flightId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long flightId) {
-
     }
 }
