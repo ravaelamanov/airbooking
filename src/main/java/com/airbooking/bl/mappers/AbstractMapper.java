de66af62f7bf3c1228fcc5bd119dc49713ef5410
ravaelamanov <ravaelamanov@gmail.com>
2021-01-31 03:41:58 +0300

    minor changes

--- src/main/java/com/airbooking/bl/mappers/AbstractMapper.java
+++ src/main/java/com/airbooking/bl/mappers/AbstractMapper.java
@@ -4,7 +4,7 @@ import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public abstract class AbstractMapper<Dto, Entity> implements IMapper<Dto, Entity>{
+public abstract class AbstractMapper<Dto, Entity> implements IMapper<Dto, Entity> {
     @Autowired
     protected ModelMapper modelMapper;
 
