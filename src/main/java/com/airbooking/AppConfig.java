3afcf7877149028c2cba323a11e980f4101583cc
ravaelamanov <ravaelamanov@gmail.com>
2021-01-31 03:47:09 +0300

    minor changes

--- src/main/java/com/airbooking/AppConfig.java
+++ src/main/java/com/airbooking/AppConfig.java
@@ -2,13 +2,9 @@ package com.airbooking;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @ComponentScan
