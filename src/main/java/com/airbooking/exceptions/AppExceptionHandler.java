55eed9cc4d83efab6107c3c8407940ce2bedc02c
ravaelamanov <ravaelamanov@gmail.com>
2021-01-31 03:47:52 +0300

    minor changes

--- src/main/java/com/airbooking/exceptions/AppExceptionHandler.java
+++ src/main/java/com/airbooking/exceptions/AppExceptionHandler.java
@@ -9,8 +9,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
