eec3cffa486022af9070b808876f84dd5ec536ff
ravaelamanov <ravaelamanov@gmail.com>
2021-02-01 18:34:13 +0300

    added mappings for seatsPriceList

--- src/main/java/com/airbooking/bl/mappers/FlightMapper.java
+++ src/main/java/com/airbooking/bl/mappers/FlightMapper.java
@@ -1,7 +1,9 @@
 package com.airbooking.bl.mappers;
 
 import com.airbooking.bl.dto.FlightDto;
+import com.airbooking.bl.dto.SeatsPriceListDto;
 import com.airbooking.da.entities.Flight;
+import com.airbooking.da.entities.SeatsPriceList;
 import com.airbooking.da.repositories.AirplaneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,17 +29,26 @@ public class FlightMapper extends AbstractMapper<FlightDto, Flight> {
     @PostConstruct
     @Override
     protected void setupMapper() {
-        modelMapper.createTypeMap(Flight.class, FlightDto.class).addMappings(m -> {m.skip(FlightDto::setAirplaneId);}).setPostConverter(entityDtoConverter());
-        modelMapper.createTypeMap(FlightDto.class, Flight.class).addMappings(m -> {m.skip(Flight::setAirplane);}).setPostConverter(dtoEntityConverter());
+        modelMapper.createTypeMap(Flight.class, FlightDto.class).
+        addMappings(m -> {m.skip(FlightDto::setAirplaneId);}).
+        addMappings(m -> {m.skip(FlightDto::setSeatsPriceListDto);}).
+        setPostConverter(entityDtoConverter());
+
+        modelMapper.createTypeMap(FlightDto.class, Flight.class).
+        addMappings(m -> {m.skip(Flight::setAirplane);}).
+        addMappings(m -> {m.skip(Flight::setSeatsPriceList);}).
+        setPostConverter(dtoEntityConverter());
     }
 
     @Override
     protected void dtoEntityCustomMapping(FlightDto flightDto, Flight flight) {
         flight.setAirplane(airplaneRepository.findById(flightDto.getAirplaneId()).orElse(null));
+        flight.setSeatsPriceList(modelMapper.map(flightDto.getSeatsPriceListDto(), SeatsPriceList.class));
     }
 
     @Override
     protected void entityDtoCustomMapping(Flight flight, FlightDto flightDto) {
         flightDto.setAirplaneId(flight.getAirplane().getId());
+        flightDto.setSeatsPriceListDto(modelMapper.map(flight.getSeatsPriceList(), SeatsPriceListDto.class));
     }
 }
