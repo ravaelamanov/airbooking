8d568981c32409a1fed5152a38754a6b20092c09
ravaelamanov <ravaelamanov@gmail.com>
2021-02-09 03:55:09 +0300

    new user is assigned ROLE_USER

--- src/main/java/com/airbooking/bl/services/UserService.java
+++ src/main/java/com/airbooking/bl/services/UserService.java
@@ -5,15 +5,20 @@ import com.airbooking.da.repositories.UserRepository;
 
 
 import com.airbooking.bl.dto.UserDto;
+import com.airbooking.security.Role;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends AbstractService<UserDto, User, Long> {
-
-
     protected UserService(@Autowired UserRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public UserDto create(UserDto userDto) {
+        User user = modelMapper.toEntity(userDto);
+        user.setRole(Role.USER);
+        return modelMapper.toDto(repository.save(user));
     }
 }
