eb0e0cca4616d9ab2f9b10d93bb3baceae1f9154
ravaelamanov <ravaelamanov@gmail.com>
2021-02-09 03:52:48 +0300

    now implements UserDetails

--- src/main/java/com/airbooking/da/entities/User.java
+++ src/main/java/com/airbooking/da/entities/User.java
@@ -1,38 +1,53 @@
 package com.airbooking.da.entities;
 
+import com.airbooking.security.Role;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "USERS")
-public class User {
-
+public class User implements UserDetails {
+    @Id
     @GeneratedValue(generator = "incrementor")
     @GenericGenerator(name = "incrementor", strategy = "increment")
     private Long id;
 
-    @Id
     @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private Boolean enabled;
-
-    @Column(nullable = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    public User() {
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getGrantedAuthorities();
     }
 
-    public Boolean getEnabled() {
-        return enabled;
+    public Role getRole() {
+        return role;
     }
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Long getId() {
@@ -43,27 +58,41 @@ public class User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String userName) {
-        this.username = userName;
-    }
-
-    public String getPassword() {
-        return password;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
+    @Override
+    public String getPassword() {
+        return password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
